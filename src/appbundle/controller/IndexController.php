4e81af58409508c6c481faad3188fb5748edd284
CedricF67 <cedric.falda@gmail.com>
2017-11-03 09:27:52 +0100

    Add controllers

---
+++ src/appbundle/controller/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Controllers;
+
+class IndexController {
+
+	public function indexAction(){
+		echo 'index';
+	}
+
+	public function contactFormAction(){
+		echo 'contactForm';
+	}
+
+}
